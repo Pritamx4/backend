b010e8856d0a71622ac674688fe806a1f1d72517
Pritamx4 <pritamsingh4092005@gmail.com>
2026-02-25 22:18:01 +0530

    add db

---
+++ src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+
+    await mongoose.connect("mongodb+srv://Pritamx4:Kalu%40409@backend.2zidbz1.mongodb.net/shit");
+
+    console.log("Connected to DB")
+
+};
+
+
+module.exports = connectDB;
