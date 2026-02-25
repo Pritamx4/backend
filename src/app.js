20ce89456e800307f6bb5146c2e462bc83db1805
Pritamx4 <pritamsingh4092005@gmail.com>
2026-02-25 23:28:41 +0530

    refine the api's according to the mongoDB

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const noteModel = require('./models/note.model');
 
 const app = express();
 
@@ -6,35 +7,48 @@ app.use(express.json());
 
 const notes = [];
 
-app.post('/notes',(req,res)=>{
-    notes.push(req.body);
+app.post('/notes', async (req,res)=>{
+    const data = req.body;
+    await noteModel.create({
+        title : data.title,
+        description : data.description
+    });
     res.status(201).json({
         message : "note created sucessfully"
-    })
+    });
 });
 
-app.get('/notes', (req,res)=>{
+app.get('/notes', async (req,res)=>{
+    const notes = await noteModel.find();
+    // const notes = await noteModel.findOne({
+    //     title: "note_title_2"
+    // });
     res.status(200).json({
         message :"notes fetched sucessfully",
         notes : notes
     });
 });
 
-app.delete('/notes/:idx',(req,res)=>{
-    const idx = req.params.idx;
-    delete notes [idx];
+app.delete('/notes/:id',async (req,res)=>{
+    const id = req.params.id;
+    await noteModel.findOneAndDelete({
+        _id : id
+    });
     res.status(200).json({
         message: "note deleted sucessfully",
     });
 });
 
-app.patch('/notes/:idx',(req,res)=>{
-    const idx = req.params.idx;
+app.patch('/notes/:id',async (req,res)=>{
+    const id = req.params.id;
     const description = req.body.description;
-    notes [idx].description = description;
-    
+    await noteModel.findOneAndUpdate({
+        _id : id
+    },{
+        description : description
+    });
     res.status(200).json({
-        message: "note updated sucessfully"
+        message: "note updated sucessfully",
     });
 });
 
--- src/db/db.js
+++ src/db/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
 
-    await mongoose.connect("mongodb+srv://Pritamx4:Kalu%40409@backend.2zidbz1.mongodb.net/shit");
-
+    await mongoose.connect("mongodb+srv://Pritamx4:Kalu%40409@backend.2zidbz1.mongodb.net/Pritamx4");
     console.log("Connected to DB")
 
 };
