41770028dc332b33a05028c970140e4ffe41faef
Pritamx4 <pritamsingh4092005@gmail.com>
2026-02-25 20:15:15 +0530

    implement CRUD operations for notes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,26 @@ app.use(express.json());
 const notes = [];
 
 app.post('/notes',(req,res)=>{
-    console.log(req.body);
+    notes.push(req.body);
+    res.status(201).json({
+        message : "note created sucessfully"
+    })
 });
 
+app.get('/notes', (req,res)=>{
+    res.status(200).json({
+        message :"notes fetched sucessfully",
+        notes : notes
+    });
+});
+
+app.delete('/notes/:idx',(req,res)=>{
+    const idx = req.params.idx;
+    delete notes [idx];
+    res.status(200).json({
+        message: "note deleted sucessfully",
+    });
+});
 
 
 module.exports = app;
